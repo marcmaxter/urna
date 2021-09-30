473ba4ae8c6c25ccb6f4aa7bb964642a0ccba76a
marco camargo <profmarcocamargo@gmail.com>
2021-09-30 18:55:16 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas= [
+    {
+        titulo:'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+            numero: '38111',
+            nome: 'FULANO de TAL',
+            partido:'ABC',
+            fotos:[
+                {url:'38111.jfif',legenda:'Vereador', small:false}
+            ]
+     },
+     {
+       numero:'77222',
+       nome: 'BELTRANO de Tal',
+       partido: 'DEFG',
+       fotos:[
+       {url:'77222.png', legenda:'Vereador'}
+     ]
+   },
+ ]
+},
+{
+    titulo:'PREFEITO',
+    numeros:2,
+    candidatos:[
+        {
+            numero:'99',
+            nome:'Ciclano',
+            partido: 'ABC',
+            vice:'Cic',
+            fotos:[
+                {url:'99.jfif', legenda:'Prefeito'},
+                {url:'99_2.png',legenda:'Vice-Prefeito', small:true}               
+            ]
+        },
+        {
+            numero:'84',
+            nome:'Zulano',
+            partido:'QWERTY',
+            vice:'Zul',
+            fotos:[
+                {url:'84.jfif', legenda:'Prefeito'},
+            {url:'84_2.jfif', legenda:'Vice-Prefeito', small:true}           
+         ]
+        },
+      ]
+    }
+];
